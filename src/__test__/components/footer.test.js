71957491a5cf490b0b92f154536c891a97cfa541
Jc <jcpablo10@gmail.com>
2021-08-13 00:08:02 -0500

    Mock styles files and mock Provider

--- src/__test__/components/footer.test.js
+++ src/__test__/components/footer.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { mount } from 'enzyme'
-import Footer from '../../components/Footer';
-import { describe } from 'yargs';
-import { expect } from '@jest/globals';
+import Footer from '../../components/Footer.jsx';
+// import { describe } from 'yargs';
+// import { expect } from '@jest/globals';
 
 describe('Probando <Footer />', () => {
+
     test('Render del componenete Footer', () => {
         const footer = mount(<Footer />);
         expect(footer.length).toEqual(1);
