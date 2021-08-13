5f4d6b47750aff8f8782c1aa513ca21c8a776a74
Jc <jcpablo10@gmail.com>
2021-08-12 23:30:04 -0500

    Configure project and create first test

---
+++ src/__test__/components/footer.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import Footer from '../../components/Footer';
+import { describe } from 'yargs';
+import { expect } from '@jest/globals';
+
+describe('Probando <Footer />', () => {
+    test('Render del componenete Footer', () => {
+        const footer = mount(<Footer />);
+        expect(footer.length).toEqual(1);
+    })
+})
